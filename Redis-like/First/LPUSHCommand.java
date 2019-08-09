7fefa7efaa5fe2d191dc4dc326e7f243e58926c2
CHSOQIWEN <44434467+CHSOQIWEN@users.noreply.github.com>
2019-08-09 17:03:37 +0800

    Redis-like

---
+++ Redis-like/First/LPUSHCommand.java
@@ -0,0 +1,12 @@
+package First;
+
+/**
+ * @Author:chaoqiwen
+ * @Date:2019/8/8 20:06
+ */
+public class LPUSHCommand implements Command {
+    @Override
+    public void run() {
+        System.out.println("我是一个lpush方法");
+    }
+}
