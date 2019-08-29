2a302286b370341761dffb8a2fe2370060c1061b
CHSOQIWEN <44434467+CHSOQIWEN@users.noreply.github.com>
2019-08-29 15:15:02 +0800

    cedis项目初

---
+++ RedisFirst/RemoteException.java
@@ -0,0 +1,27 @@
+package First;
+
+/**
+ * @Author:chaoqiwen
+ * @Date:2019/8/8 16:50
+ */
+public class RemoteException extends  AllException{
+    public RemoteException() {
+
+    }
+
+    public RemoteException(String message) {
+        super(message);
+    }
+
+    public RemoteException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public RemoteException(Throwable cause) {
+        super(cause);
+    }
+
+    protected RemoteException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
